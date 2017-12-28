c9ed56c47ec31f23e2a5613bbb78079a19f0fc70
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 11:05:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,3 @@
-/*var playlist = {'Paul Simon': 'American Tune'};
-
-function updatePlaylist(playlist, artistName, song) {
-  playlist[artistName] = song;
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName) {
-delete playlist[artistName]
-return playlist;
-}
-*/
 var playlist = {artistName: 'playlist'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
