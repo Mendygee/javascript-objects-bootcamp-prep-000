ec3242385ab645b654f2656f798c7623594d7db1
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 11:04:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -17,7 +17,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName, songTitle) {
-  delete artistName.songTitle;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 }
